b3fb9234229508c2453f966cad3394711cd8ff01
abdelilahraees <abdelilahraees@gmail.com>
2025-08-28 12:51:59 +0100

    firstCommit

---
+++ src/test/java/com/kida/ga/GaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kida.ga;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
