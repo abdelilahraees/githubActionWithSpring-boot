c054255489f1abe1a2124bc77b714facb2c845ea
abdelilahraees <abdelilahraees@gmail.com>
2025-08-30 14:28:17 +0100

    deploy

---
+++ src/main/java/com/kida/ga/UserController.java
@@ -0,0 +1,12 @@
+package com.kida.ga;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class UserController {
+    @GetMapping("/")
+    public  String index() {
+        return "Hello World!";
+    }
+}
